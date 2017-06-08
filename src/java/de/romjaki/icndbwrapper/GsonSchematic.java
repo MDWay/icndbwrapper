4eb7aa5d07f4fe3720a74f6518d7f8b70544901f
mdway <romangraef@gmail.com>
2017-06-08 17:07:33 +0200

    Initial Commit

---
+++ src/java/de/romjaki/icndbwrapper/GsonSchematic.java
@@ -0,0 +1,8 @@
+package de.romjaki.icndbwrapper;
+
+/**
+ * Created by RGR on 08.06.2017.
+ */
+public abstract class GsonSchematic<T> {
+
+}
--- src/java/de/romjaki/icndbwrapper/RestAction.java
+++ src/java/de/romjaki/icndbwrapper/RestAction.java
@@ -1,42 +1,44 @@
 package de.romjaki.icndbwrapper;
 
-public class RestAction<T, R extends GsonSchematic<T>> {
-	
-	private final R emptySchematic;
-	private final String url;
-	private final Map<String, String> parameters;
-	
-	public RestAction(R emptySchematic, String relativeUrl, Map<String, String> parameters) {
-		this.emptySchematic = emptySchematic;
-		this.url = relativeUrl;
-		this.parameters = parameters;
-	}
-	
-	public void queue() {
-		queue(null);
-	}
-	
-	public void queue(Consumer<T> success) {
-		queue(success, null);
-	}
-	
-	public void queue(Consumer<T> success, Consumer<T> fail) {
-		Thread t = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Gson gson = new Gson();
-				R schematic = gson.fromJson(executeGet(this.url, this.parameters), emptySchematic.getClass());
-				//TODO
-			}
-		});
-	}
-	
-	public T complete() {
-		// TODO
-	}
-	
-	public static String executeGet(String url, Map<String, String> parameters) {
-		// TODO
-	}
-	
+import com.google.gson.Gson;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+
+public class RestAction<T> {
+
+    private final GsonSchematic<T> emptySchematic;
+    private final String url;
+    private final Map<String, String> parameters;
+
+    public RestAction(R emptySchematic, String relativeUrl, Map<String, String> parameters) {
+        this.emptySchematic = emptySchematic;
+        this.url = relativeUrl;
+        this.parameters = parameters;
+    }
+
+    public static String executeGet(String url, Map<String, String> parameters) {
+        return "";
+    }
+
+    public void queue() {
+        queue(null);
+    }
+
+    public void queue(Consumer<T> success) {
+        queue(success, null);
+    }
+
+    public void queue(Consumer<T> success, Consumer<T> fail) {
+        Thread t = new Thread(() -> {
+            Gson gson = new Gson();
+            R schematic =  gson.fromJson(executeGet(RestAction.this.url, RestAction.this.parameters), emptySchematic.getClass());
+        });
+    }
+
+    public T complete() {
+        // TODO
+    }
+
 }
